c24ac0aac43acfb777303862c8a6bdc4c7f23870
YonatanReuvenIsraeli <156263968+YonatanReuvenIsraeli@users.noreply.github.com>
2024-01-11 11:38:20 -0500

    Updated Windows To Go Creation Tool.bat
    Updated batch file from 350 lines of code (14KB) to 213 lines of code (9KB) while keeping the exact same functionality.

--- Windows To Go Creation Tool.bat
+++ Windows To Go Creation Tool.bat
@@ -208,6 +208,6 @@ goto :Done
 :Done
 bcdboot %WindowsDriveLetter%\Windows /s %FAT32% /f ALL
 echo.
-echo Your Windows To Go is ready. It is Bootable with Legacy BIOS and UEFI. Press any key to exit Windows to Go Creation Tool.
+echo Your Windows To Go is ready. It is Bootable with Legacy BIOS and UEFI. Press any key to exit Windows To Go Creation Tool.
 endlocal
 pause
