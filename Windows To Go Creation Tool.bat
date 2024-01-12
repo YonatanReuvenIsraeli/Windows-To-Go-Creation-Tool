5ef1aad4b4125f8c447a8dea94f620c7f9e87fe0
YonatanReuvenIsraeli <156263968+YonatanReuvenIsraeli@users.noreply.github.com>
2024-01-11 22:24:30 -0500

    Update Windows To Go Creation Tool.bat

--- Windows To Go Creation Tool.bat
+++ Windows To Go Creation Tool.bat
@@ -16,7 +16,7 @@ pause
 
 :Start
 echo.
-set /p install="Do you have install.esd or install.wim? (install.esd/install.wim) "
+set /p "install"="Do you have install.esd or install.wim? (install.esd/install.wim) "
 if /i "%install%"=="install.esd" goto :DriveLetter
 if /i "%install%"=="install.wim" goto :DriveLetter
 echo Invalid Syntax!
@@ -24,7 +24,7 @@ goto :Start
 
 :DriveLetter
 echo.
-set /p DriveLetter="What is your drive letter of you mounted Windows Disk Image? (A:-Z:) "
+set /p "DriveLetter"="What is your drive letter of you mounted Windows Disk Image? (A:-Z:) "
 if /i "%DriveLetter%"=="A:" goto :SureDriveLetter
 if /i "%DriveLetter%"=="B:" goto :SureDriveLetter
 if /i "%DriveLetter%"=="C:" goto :SureDriveLetter
@@ -56,7 +56,7 @@ goto :DriveLetter
 
 :SureDriveLetter
 echo.
-set /p SureNumber="Are you sure %DriveLetter% is the drive letter of your Windows Disk Image? (Yes/No) "
+set /p "SureNumber"="Are you sure %DriveLetter% is the drive letter of your Windows Disk Image? (Yes/No) "
 if /i "%SureNumber%"=="Yes" goto :ESDWIM1
 if /i "%SureNumber%"=="No" goto :DriveLetter
 echo Invalid Syntax!
@@ -76,7 +76,7 @@ goto :Index
 
 :Index
 echo.
-set /p Index="Which index do you want? (1-7/11) "
+set /p "Index"="Which index do you want? (1-7/11) "
 if /i "%Index%"=="1" goto :SureIndex
 if /i "%Index%"=="2" goto :SureIndex
 if /i "%Index%"=="3" goto :SureIndex
@@ -94,7 +94,7 @@ goto :Index
 
 :SureIndex
 echo.
-set /p IndexNumber="Are you sure you want Index %Index%? (Yes/No) "
+set /p "IndexNumber"="Are you sure you want Index %Index%? (Yes/No) "
 if /i "%IndexNumber%"=="Yes" goto :Create
 if /i "%IndexNumber%"=="No" goto :Index
 echo Invalid Syntax!
@@ -112,51 +112,51 @@ echo Step 5. Create on disk you want to install Windows on one NTFS partition th
 echo Step 6. Make the FAT32 partition on the drive Windows is installed on active.
 echo Step 7. Press any key to continue.
 pause
-goto :WindowsDriveLetter
+goto :NTFS
 
-:WindowsDriveLetter
+:NTFS
 echo.
-set /p WindowsDriveLetter="What is your drive letter of the NTFS partition you want to install windows on? (A:-Z:) "
-if /i "%WindowsDriveLetter%"=="A:" goto :SureNTFS
-if /i "%WindowsDriveLetter%"=="B:" goto :SureNTFS
-if /i "%WindowsDriveLetter%"=="C:" goto :SureNTFS
-if /i "%WindowsDriveLetter%"=="D:" goto :SureNTFS
-if /i "%WindowsDriveLetter%"=="E:" goto :SureNTFS
-if /i "%WindowsDriveLetter%"=="F:" goto :SureNTFS
-if /i "%WindowsDriveLetter%"=="G:" goto :SureNTFS
-if /i "%WindowsDriveLetter%"=="H:" goto :SureNTFS
-if /i "%WindowsDriveLetter%"=="I:" goto :SureNTFS
-if /i "%WindowsDriveLetter%"=="J:" goto :SureNTFS
-if /i "%WindowsDriveLetter%"=="K:" goto :SureNTFS
-if /i "%WindowsDriveLetter%"=="L:" goto :SureNTFS
-if /i "%WindowsDriveLetter%"=="M:" goto :SureNTFS
-if /i "%WindowsDriveLetter%"=="N:" goto :SureNTFS
-if /i "%WindowsDriveLetter%"=="O:" goto :SureNTFS
-if /i "%WindowsDriveLetter%"=="P:" goto :SureNTFS
-if /i "%WindowsDriveLetter%"=="Q:" goto :SureNTFS
-if /i "%WindowsDriveLetter%"=="R:" goto :SureNTFS
-if /i "%WindowsDriveLetter%"=="S:" goto :SureNTFS
-if /i "%WindowsDriveLetter%"=="T:" goto :SureNTFS
-if /i "%WindowsDriveLetter%"=="U:" goto :SureNTFS
-if /i "%WindowsDriveLetter%"=="V:" goto :SureNTFS
-if /i "%WindowsDriveLetter%"=="W:" goto :SureNTFS
-if /i "%WindowsDriveLetter%"=="X:" goto :SureNTFS
-if /i "%WindowsDriveLetter%"=="Y:" goto :SureNTFS
-if /i "%WindowsDriveLetter%"=="Z:" goto :SureNTFS
+set /p "NTFS"="What is your drive letter of the NTFS partition you want to install windows on? (A:-Z:) "
+if /i "%NTFS%"=="A:" goto :SureNTFS
+if /i "%NTFS%"=="B:" goto :SureNTFS
+if /i "%NTFS%"=="C:" goto :SureNTFS
+if /i "%NTFS%"=="D:" goto :SureNTFS
+if /i "%NTFS%"=="E:" goto :SureNTFS
+if /i "%NTFS%"=="F:" goto :SureNTFS
+if /i "%NTFS%"=="G:" goto :SureNTFS
+if /i "%NTFS%"=="H:" goto :SureNTFS
+if /i "%NTFS%"=="I:" goto :SureNTFS
+if /i "%NTFS%"=="J:" goto :SureNTFS
+if /i "%NTFS%"=="K:" goto :SureNTFS
+if /i "%NTFS%"=="L:" goto :SureNTFS
+if /i "%NTFS%"=="M:" goto :SureNTFS
+if /i "%NTFS%"=="N:" goto :SureNTFS
+if /i "%NTFS%"=="O:" goto :SureNTFS
+if /i "%NTFS%"=="P:" goto :SureNTFS
+if /i "%NTFS%"=="Q:" goto :SureNTFS
+if /i "%NTFS%"=="R:" goto :SureNTFS
+if /i "%NTFS%"=="S:" goto :SureNTFS
+if /i "%NTFS%"=="T:" goto :SureNTFS
+if /i "%NTFS%"=="U:" goto :SureNTFS
+if /i "%NTFS%"=="V:" goto :SureNTFS
+if /i "%NTFS%"=="W:" goto :SureNTFS
+if /i "%NTFS%"=="X:" goto :SureNTFS
+if /i "%NTFS%"=="Y:" goto :SureNTFS
+if /i "%NTFS%"=="Z:" goto :SureNTFS
 echo Invalid Syntax!
-goto :WindowsDriveLetter
+goto :NTFS
 
 :SureNTFS
 echo.
-set /p SureNumber="Are you sure %WindowsDriveLetter% is the drive letter you want to install Windows on? (Yes/No) "
+set /p "SureNumber"="Are you sure %NTFS% is the drive letter you want to install Windows on? (Yes/No) "
 if /i "%SureNumber%"=="Yes" goto :BCD
-if /i "%SureNumber%"=="No" goto :WindowsDriveLetter
+if /i "%SureNumber%"=="No" goto :NTFS
 echo Invalid Syntax!
 goto :SureNTFS
 
 :BCD
 echo.
-set /p FAT32="What is your drive letter of the FAT32 partition on the drive you want to install windows on? (A:-Z:) "
+set /p "FAT32"="What is your drive letter of the FAT32 partition on the drive you want to install windows on? (A:-Z:) "
 if /i "%FAT32%"=="A:" goto :SureFAT32
 if /i "%FAT32%"=="B:" goto :SureFAT32
 if /i "%FAT32%"=="C:" goto :SureFAT32
@@ -188,7 +188,7 @@ goto :BCD
 
 :SureFAT32
 echo.
-set /p SureNumber="Are you sure %FAT32% is the drive letter you wan to install Windows on? (Yes/No) "
+set /p "SureNumber"="Are you sure %FAT32% is the drive letter you wan to install Windows on? (Yes/No) "
 if /i "%SureNumber%"=="Yes" goto :ESDWIM2
 if /i "%SureNumber%"=="No" goto :BCD
 echo Invalid Syntax!
@@ -199,15 +199,15 @@ if /i "%install%"=="install.esd" goto :DISMESD2
 if /i "%install%"=="install.wim" goto :DISMWIM2
 
 :DISMESD2
-DISM /Apply-Image /ImageFile:"%DriveLetter%\sources\install.esd" /Index:%Index% /ApplyDir:%WindowsDriveLetter% || goto :IndexESD
+DISM /Apply-Image /ImageFile:"%DriveLetter%\sources\install.esd" /Index:%Index% /ApplyDir:%NTFS% || goto :IndexESD
 goto :Done
 
 :DISMWIM2
-DISM/Apply-Image /ImageFile:"%DriveLetter%\sources\install.wim" /Index:%Index% /ApplyDir:%WindowsDriveLetter% || goto :IndexWIM
+DISM/Apply-Image /ImageFile:"%DriveLetter%\sources\install.wim" /Index:%Index% /ApplyDir:%NTFS% || goto :IndexWIM
 goto :Done
 
 :Done
-BCDBOOT %WindowsDriveLetter%\Windows /s %FAT32% /f ALL
+BCDBOOT %NTFS%\Windows /s %FAT32% /f ALL
 echo.
 echo Your Windows To Go is ready. It is Bootable with Legacy BIOS and UEFI. Press any key to exit Windows To Go Creation Tool.
 endlocal
