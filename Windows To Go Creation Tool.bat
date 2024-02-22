b605f7fec80c9716de09b8beed926db875445726
Yonatan Israeli <yonirisraeli@gmail.com>
2024-02-22 10:19:42 -0500

    Update Windows To Go Creation Tool.bat

--- Windows To Go Creation Tool.bat
+++ Windows To Go Creation Tool.bat
@@ -11,7 +11,7 @@ echo.
 echo Please mount your Windows Disk Image then press any key to continue.
 pause
 echo. 
-echo Go to X:\sources, where X: is the drive letter of you monted disk image and look for install.esd or install.wim and then press any key to continue.
+echo Go to X:\sources, where X: is the drive letter of you monted disk image and look for install.esd or install.swm or install.wim and then press any key to continue.
 pause
 goto :Start
 
