3553f2fa822cb437a89f3f659f6d9e6ba1a0b6a5
Yonatan Israeli <yonirisraeli@gmail.com>
2024-12-19 18:55:27 -0500

    Update Windows Installation Tool.bat

--- Windows Installation Tool.bat
+++ Windows Installation Tool.bat
@@ -2,10 +2,11 @@
 setlocal
 title Windows Installation Tool
 echo Program Name: Windows Installation Tool
-echo Version: 5.0.2
-echo Developer: @YonatanReuvenIsraeli
-echo Website: https://www.yonatanreuvenisraeli.dev
+echo Version: 5.0.3
 echo License: GNU General Public License v3.0
+echo Developer: @YonatanReuvenIsraeli
+echo GitHub: https://github.com/YonatanReuvenIsraeli
+echo Sponsor: https://github.com/sponsors/YonatanReuvenIsraeli 
 net session > nul 2>&1
 if not "%errorlevel%"=="0" goto "NotAdministrator"
 goto "Start"
